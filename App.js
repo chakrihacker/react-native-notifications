54d79cc4e0bd3e7a2c7696ceac19d235d5394edd
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2018-11-20 20:30:16 +0530

    Ask for permissions

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Notifications, Permissions } from "expo";
 
 export default class App extends React.Component {
+  askPermissions = async () => {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      return false;
+    }
+    return true;
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          title="Please accept Notifications Permissions"
+          onPress={() => this.askPermissions()}
+        />
       </View>
     );
   }
@@ -14,8 +33,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
