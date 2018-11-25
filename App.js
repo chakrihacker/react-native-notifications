e0cbc52aadaf0fdc4ba3ef05a77d0fb48aa73543
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2018-11-25 12:50:01 +0530

    Push notification

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Clipboard } from "react-native";
 import { Notifications, Permissions } from "expo";
 
 export default class App extends React.Component {
+  state = {
+    token: "",
+    data: null,
+    origin: null
+  };
+
   askPermissions = async () => {
     const { status: existingStatus } = await Permissions.getAsync(
       Permissions.NOTIFICATIONS
@@ -40,6 +46,38 @@ export default class App extends React.Component {
     console.log(notificationId);
   };
 
+  registerForPushNotifications = async () => {
+    const enabled = await this.askPermissions();
+    if (!enabled) {
+      return Promise.resolve();
+    }
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+    return token;
+  };
+
+  enablePushNotifications = async () => {
+    let token = await this.registerForPushNotifications();
+    if (token) {
+      Clipboard.setString(token);
+      this.setState({ token });
+    }
+  };
+
+  componentDidMount = () => {
+    this.notificationSubscription = Notifications.addListener(
+      this.handlePushNotification
+    );
+  };
+
+  componentWillUnmount = () => {
+    this.notificationSubscription.remove();
+  };
+
+  handlePushNotification = ({ origin, data }) => {
+    this.setState({ data, origin });
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -63,6 +101,16 @@ export default class App extends React.Component {
           title="Cancel Scheduled Notifications"
           onPress={() => Notifications.cancelAllScheduledNotificationsAsync()}
         />
+        {this.state.token.length === 0 ? (
+          <Button
+            title={"Get Push Token"}
+            onPress={this.enablePushNotifications}
+          />
+        ) : (
+          <Text>{this.state.token}</Text>
+        )}
+        <Text>{this.state.origin && this.state.origin}</Text>
+        <Text>{this.state.data && this.state.data}</Text>
       </View>
     );
   }
